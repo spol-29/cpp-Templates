dc47ca7b49a153914a2856e8791570d9a496412f
spol-29 <polineni.sriram9@gmail.com>
2023-02-19 16:42:27 -0600

    templates

---
+++ combo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<array>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main(){
+    
+}
---
+++ include.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<array>
+#include<algorithm>
+
+int main(){
+
+}
